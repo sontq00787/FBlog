35140f046d3020db918a4bb2ef1f3cd20f4a5860
sontq00787 <sontq00787@gmail.com>
2014-12-10 20:23:04 +0700

    Move file

--- category_management.php
+++ category_management.php
@@ -13,71 +13,71 @@
     <!-- Link shortcut icon-->
     <link rel="shortcut icon" type="image/ico" href="images/favicon2.html" /> 
     <!-- Link css-->
-    <link rel="stylesheet" type="text/css" href="css/zice.style.css"/>
-    <link rel="stylesheet" type="text/css" href="css/icon.css"/>
-    <link rel="stylesheet" type="text/css" href="css/ui-custom.css"/>
-    <link rel="stylesheet" type="text/css" href="css/timepicker.css"  />
-    <link rel="stylesheet" type="text/css" href="components/colorpicker/css/colorpicker.css"  />
-    <link rel="stylesheet" type="text/css" href="components/elfinder/css/elfinder.css" />
-    <link rel="stylesheet" type="text/css" href="components/datatables/dataTables.css"  />
-    <link rel="stylesheet" type="text/css" href="components/validationEngine/validationEngine.jquery.css" />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/css/zice.style.css"/>
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/css/icon.css"/>
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/css/ui-custom.css"/>
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/css/timepicker.css"  />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/colorpicker/css/colorpicker.css"  />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/elfinder/css/elfinder.css" />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/datatables/dataTables.css"  />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/validationEngine/validationEngine.jquery.css" />
      
-    <link rel="stylesheet" type="text/css" href="components/jscrollpane/jscrollpane.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="components/fancybox/jquery.fancybox.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="components/tipsy/tipsy.css" media="all" />
-    <link rel="stylesheet" type="text/css" href="components/editor/jquery.cleditor.css"  />
-    <link rel="stylesheet" type="text/css" href="components/chosen/chosen.css" />
-    <link rel="stylesheet" type="text/css" href="components/confirm/jquery.confirm.css" />
-    <link rel="stylesheet" type="text/css" href="components/sourcerer/sourcerer.css"/>
-    <link rel="stylesheet" type="text/css" href="components/fullcalendar/fullcalendar.css"/>
-    <link rel="stylesheet" type="text/css" href="components/Jcrop/jquery.Jcrop.css"  />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/jscrollpane/jscrollpane.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/fancybox/jquery.fancybox.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/tipsy/tipsy.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/editor/jquery.cleditor.css"  />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/chosen/chosen.css" />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/confirm/jquery.confirm.css" />
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/sourcerer/sourcerer.css"/>
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/fullcalendar/fullcalendar.css"/>
+    <link rel="stylesheet" type="text/css" href="styles/ziceadmin/components/Jcrop/jquery.Jcrop.css"  />
 
     
     <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="components/flot/excanvas.min.js"></script><![endif]-->
     
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="components/ui/jquery.ui.min.js"></script> 
-    <script type="text/javascript" src="components/ui/jquery.autotab.js"></script>
-    <script type="text/javascript" src="components/ui/timepicker.js"></script>
-    <script type="text/javascript" src="components/colorpicker/js/colorpicker.js"></script>
-    <script type="text/javascript" src="components/checkboxes/iphone.check.js"></script>
-    <script type="text/javascript" src="components/elfinder/js/elfinder.full.js"></script>
-    <script type="text/javascript" src="components/datatables/dataTables.min.js"></script>
-    <script type="text/javascript" src="components/datatables/ColVis.js"></script>
-    <script type="text/javascript" src="components/scrolltop/scrolltopcontrol.js"></script>
-    <script type="text/javascript" src="components/fancybox/jquery.fancybox.js"></script>
-    <script type="text/javascript" src="components/jscrollpane/mousewheel.js"></script>
-    <script type="text/javascript" src="components/jscrollpane/mwheelIntent.js"></script>
-    <script type="text/javascript" src="components/jscrollpane/jscrollpane.min.js"></script>
-    <script type="text/javascript" src="components/spinner/ui.spinner.js"></script>
-    <script type="text/javascript" src="components/tipsy/jquery.tipsy.js"></script>
-    <script type="text/javascript" src="components/editor/jquery.cleditor.js"></script>
-    <script type="text/javascript" src="components/chosen/chosen.js"></script>
-    <script type="text/javascript" src="components/confirm/jquery.confirm.js"></script>
-    <script type="text/javascript" src="components/validationEngine/jquery.validationEngine.js" ></script>
-    <script type="text/javascript" src="components/validationEngine/jquery.validationEngine-en.js" ></script>
-    <script type="text/javascript" src="components/vticker/jquery.vticker-min.js"></script>
-    <script type="text/javascript" src="components/sourcerer/sourcerer.js"></script>
-    <script type="text/javascript" src="components/fullcalendar/fullcalendar.js"></script>
-    <script type="text/javascript" src="components/flot/flot.js"></script>
-    <script type="text/javascript" src="components/flot/flot.pie.min.js"></script>
-    <script type="text/javascript" src="components/flot/flot.resize.min.js"></script>
-    <script type="text/javascript" src="components/flot/graphtable.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/js/jquery.min.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/ui/jquery.ui.min.js"></script> 
+    <script type="text/javascript" src="styles/ziceadmin/components/ui/jquery.autotab.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/ui/timepicker.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/colorpicker/js/colorpicker.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/checkboxes/iphone.check.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/elfinder/js/elfinder.full.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/datatables/dataTables.min.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/datatables/ColVis.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/scrolltop/scrolltopcontrol.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/fancybox/jquery.fancybox.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/jscrollpane/mousewheel.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/jscrollpane/mwheelIntent.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/jscrollpane/jscrollpane.min.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/spinner/ui.spinner.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/tipsy/jquery.tipsy.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/editor/jquery.cleditor.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/chosen/chosen.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/confirm/jquery.confirm.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/validationEngine/jquery.validationEngine.js" ></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/validationEngine/jquery.validationEngine-en.js" ></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/vticker/jquery.vticker-min.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/sourcerer/sourcerer.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/fullcalendar/fullcalendar.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/flot/flot.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/flot/flot.pie.min.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/flot/flot.resize.min.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/flot/graphtable.js"></script>
 
-    <script type="text/javascript" src="components/uploadify/swfobject.js"></script>
-    <script type="text/javascript" src="components/uploadify/uploadify.js"></script>        
-    <script type="text/javascript" src="components/checkboxes/customInput.jquery.js"></script>
-    <script type="text/javascript" src="components/effect/jquery-jrumble.js"></script>
-    <script type="text/javascript" src="components/filestyle/jquery.filestyle.js" ></script>
-    <script type="text/javascript" src="components/placeholder/jquery.placeholder.js" ></script>
-	<script type="text/javascript" src="components/Jcrop/jquery.Jcrop.js" ></script>
-    <script type="text/javascript" src="components/imgTransform/jquery.transform.js" ></script>
-    <script type="text/javascript" src="components/webcam/webcam.js" ></script>
-	<script type="text/javascript" src="components/rating_star/rating_star.js"></script>
-	<script type="text/javascript" src="components/dualListBox/dualListBox.js"  ></script>
-	<script type="text/javascript" src="components/smartWizard/jquery.smartWizard.min.js"></script>
-    <script type="text/javascript" src="js/jquery.cookie.js"></script>
-    <script type="text/javascript" src="js/zice.custom.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/uploadify/swfobject.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/uploadify/uploadify.js"></script>        
+    <script type="text/javascript" src="styles/ziceadmin/components/checkboxes/customInput.jquery.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/effect/jquery-jrumble.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/filestyle/jquery.filestyle.js" ></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/placeholder/jquery.placeholder.js" ></script>
+	<script type="text/javascript" src="styles/ziceadmin/components/Jcrop/jquery.Jcrop.js" ></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/imgTransform/jquery.transform.js" ></script>
+    <script type="text/javascript" src="styles/ziceadmin/components/webcam/webcam.js" ></script>
+	<script type="text/javascript" src="styles/ziceadmin/components/rating_star/rating_star.js"></script>
+	<script type="text/javascript" src="styles/ziceadmin/components/dualListBox/dualListBox.js"  ></script>
+	<script type="text/javascript" src="styles/ziceadmin/components/smartWizard/jquery.smartWizard.min.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/js/jquery.cookie.js"></script>
+    <script type="text/javascript" src="styles/ziceadmin/js/zice.custom.js"></script>
 	<style type="text/css">
 		.table-row{
 			border: 1px solid #000;
@@ -97,9 +97,9 @@
                        
     <div id="header" >
         <div id="account_info"> 
-            <img src="images/avatar.png" alt="Online" class="avatar" />
-			<div class="setting" title="Profile Setting"><b>Welcome,</b> <b class="red">John Doe</b><img src="images/gear.png" class="gear"  alt="Profile Setting" ></div>
-			<div class="logout" title="Disconnect"><b >Logout</b> <img src="images/connect.png" name="connect" class="disconnect" alt="disconnect" ></div> 
+            <img src="styles/ziceadmin/images/avatar.png" alt="Online" class="avatar" />
+			<div class="setting" title="Profile Setting"><b>Welcome,</b> <b class="red">John Doe</b><img src="styles/ziceadmin/images/gear.png" class="gear"  alt="Profile Setting" ></div>
+			<div class="logout" title="Disconnect"><b >Logout</b> <img src="styles/ziceadmin/images/connect.png" name="connect" class="disconnect" alt="disconnect" ></div> 
         </div>
     </div> <!--//  header -->
 	
@@ -155,10 +155,10 @@
         	<div class="topcolumn">
 				<div class="logo"></div>
                     <ul  id="shortcut">
-                        <li> <a href="#" title="Back To home"> <img src="images/icon/shortcut/home.png" alt="home"/><strong>Home</strong> </a> </li>
-                        <li> <a href="#" title="Website Graph"> <img src="images/icon/shortcut/graph.png" alt="graph"/><strong>Graph</strong> </a> </li>
-                        <li> <a href="#" title="Setting" > <img src="images/icon/shortcut/setting.png" alt="setting" /><strong>Setting</strong></a> </li> 
-                        <li> <a href="#" title="Messages"> <img src="images/icon/shortcut/mail.png" alt="messages" /><strong>Message</strong></a><div class="notification" >10</div></li>
+                        <li> <a href="#" title="Back To home"> <img src="styles/ziceadmin/images/icon/shortcut/home.png" alt="home"/><strong>Home</strong> </a> </li>
+                        <li> <a href="#" title="Website Graph"> <img src="styles/ziceadmin/images/icon/shortcut/graph.png" alt="graph"/><strong>Graph</strong> </a> </li>
+                        <li> <a href="#" title="Setting" > <img src="styles/ziceadmin/images/icon/shortcut/setting.png" alt="setting" /><strong>Setting</strong></a> </li> 
+                        <li> <a href="#" title="Messages"> <img src="styles/ziceadmin/images/icon/shortcut/mail.png" alt="messages" /><strong>Message</strong></a><div class="notification" >10</div></li>
                     </ul>
 			</div>
 			<table class="table-row display static dataTable" id="static">
